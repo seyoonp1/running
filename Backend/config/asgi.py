bd993802d5abc04b78eef825d6baed33232fa984
seyoonp1 <seyoonp1@gmail.com>
2026-01-27 13:06:54 +0900

    사소한 오류

--- Backend/config/asgi.py
+++ Backend/config/asgi.py
@@ -4,7 +4,6 @@ ASGI config for hexgame backend.
 import os
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from apps.realtime.middleware import JWTAuthMiddlewareStack
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
@@ -12,8 +11,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
 
-# Import routing after Django is initialized
+# Import routing and middleware after Django is initialized
 from apps.realtime import routing as realtime_routing
+from apps.realtime.middleware import JWTAuthMiddlewareStack
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
